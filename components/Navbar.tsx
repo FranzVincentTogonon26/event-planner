ece48cb8310f06180e1194c79dbd78a4d5e52853
Jhen2490 <franzvincent@gmail.com>
2026-07-03 14:09:33 +0800

    feat:set-up Neon Auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar(){
     return (
         <nav className="bg-slate-800 border-b border-slate-700 shadow-lg">
             <div className="max-w-7xl mx-auto px-4">
-                <div className="h-16 flex justify-between">
+                <div className="h-16 flex justify-between"> 
                     <div className="flex items-center">
                         <Link href="/" className="text-primary text-xl font-bold">
                             Event Planner
